ebcca1be589966c97486cd31621fe61197cf9183
kirubha <developer.datasense@gmail.com>
2021-08-06 11:59:34 +0530

    initial commit

---
+++ app/src/main/java/com/retailvend/DashboardAdapter.java
@@ -0,0 +1,4 @@
+package com.retailvend;
+
+public class DashboardAdapter {
+}
---
+++ app/src/main/java/com/retailvend/MainActivity.java
@@ -0,0 +1,25 @@
+package com.retailvend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+}
