8b9328187f245b838283d055ca6d2a8fd3998428
Nivetha789 <nivethasai777@gmail.com>
2021-09-01 19:43:36 +0530

    sales order completed 1-09-2021

---
+++ app/src/main/java/com/retailvend/sales/SalesAdapter.java
@@ -0,0 +1,76 @@
+package com.retailvend.sales;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.retailvend.R;
+
+public class SalesAdapter extends RecyclerView.Adapter<SalesAdapter.MyViewHolder> {
+
+    private static final int VIEW_TYPE_LOADING = 0;
+    private static final int VIEW_TYPE_NORMAL = 1;
+    private static boolean isLoaderVisible = false;
+    //    private List<Saleslist> saleslists;
+    private Activity activity;
+
+
+//    public SalesAdapter(Context context, List<Saleslist> postItems) {
+//        this.saleslists = postItems;
+//        this.context = context;
+//    }
+
+    SalesAdapter(Activity activity) {
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public SalesAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(activity).inflate(R.layout.item_sales_list, viewGroup, false);
+        return new SalesAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+//        BuyEShareListData data = datas.get(position);
+//        holder.cardView.setTag(data);
+//        String value = "$ " + data.getEsPrice();
+//        holder.cost.setText(value);
+//        if (data.getEsName().equalsIgnoreCase("Gold")) {
+//            holder.backgroundImg.setBackground(ContextCompat.getDrawable(activity, R.drawable.gold));
+//        } else if (data.getEsName().equalsIgnoreCase("Silver")) {
+//            holder.backgroundImg.setBackground(ContextCompat.getDrawable(activity, R.drawable.silver));
+//        } else if (data.getEsName().equalsIgnoreCase("diamond")) {
+//            holder.backgroundImg.setBackground(ContextCompat.getDrawable(activity, R.drawable.diamond));
+//        } else if (data.getEsName().equalsIgnoreCase("Platinum")) {
+//            holder.backgroundImg.setBackground(ContextCompat.getDrawable(activity, R.drawable.platinum));
+//        } else if (data.getEsName().equalsIgnoreCase("Bronze")) {
+//            holder.backgroundImg.setBackground(ContextCompat.getDrawable(activity, R.drawable.bronze));
+//        }
+//        if((datas.size()-1)==position){
+//            holder.view.setVisibility(View.VISIBLE);
+//        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+//    void setOnClickListener(OnClickListener onClickListener) {
+//        this.onClickListener = onClickListener;
+//    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        MyViewHolder(View itemView) {
+            super(itemView);
+        }
+
+    }
+}
